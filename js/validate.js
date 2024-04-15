0f3d4749c54f2a0fcc6c16df294574c7cce767c7
Your Name <motsisitheo@gmail.com>
2024-04-15 12:13:39 +0200

    added validate method to form

---
+++ js/validate.js
@@ -0,0 +1,20 @@
+function validate() {
+
+    // Fixed variable
+    let name = "Theo";
+
+    // Dynamic variable
+    let fname = document.getElementById('fname').value;
+    let lname = document.getElementById('lname').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
+    let dob = document.getElementById('dob').value;
+
+    // Conditional statement and logic operator
+    if (fname == "") {
+        alert("First name is required");
+        return false;
+    }
+    alert("User details saved successfully!");
+    return true;
+}
