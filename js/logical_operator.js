4c669c75d8c4241cc2d30fcde7908f5d96980e2a
Your Name <motsisitheo@gmail.com>
2024-04-15 11:45:22 +0200

    added form to contact us

--- js/logical_operator.js
+++ js/logical_operator.js
@@ -1,10 +1,12 @@
 function isTheo(){
-    let age = 30;
+    let age = 31;
+    let random_number = 31;
     let name = "Theo";
 
-    if(age > 18){
+    if(age != random_number){
         console.log(name + " is old enough to drive");
     }else{
         console.log(name + " is not old enough to drive");
     }
 }
+
