5991f8b6d3e56ad9a7f0037964983537db5f9673
Your Name <motsisitheo@gmail.com>
2024-04-15 11:34:22 +0200

    added logical operator

---
+++ js/logical_operator.js
@@ -0,0 +1,10 @@
+function isTheo(){
+    let age = 30;
+    let name = "Theo";
+
+    if(age > 18){
+        console.log(name + " is old enough to drive");
+    }else{
+        console.log(name + " is not old enough to drive");
+    }
+}
